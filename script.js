c3d6fc3a20fe8800f30a6a6937db411dfa729a4a
Ethan Frot <148550084+ethan-frot@users.noreply.github.com>
2023-12-18 00:51:51 +0100

    Version 2
    I change my script to optimize my swiper and added some cards

--- script.js
+++ script.js
@@ -1,49 +1,20 @@
-const navItems = document.querySelectorAll(".nav-item");
-const selector = document.querySelector(".selector");
+// Variables
 const chevronDown = document.querySelector(".chevron-down");
-const chevronLeft = document.querySelector(".chevron-left");
-const chevronRight = document.querySelector(".chevron-right");
-const cardContainer = document.querySelectorAll(".card-container");
-const cardContentContainer = document.querySelectorAll(
-  ".card-content-container"
-);
 
-const cards = Array.from(document.querySelectorAll(".card"));
-
-let cardIndex = 2;
-
-function hideCard() {
-  cardContentContainer[cardIndex].classList.add("hidden");
-  cardContainer[cardIndex].classList.add("hide-card-container");
-}
-
-function showCard() {
-  cardContentContainer[cardIndex].classList.remove("hidden");
-  cardContainer[cardIndex].classList.remove("hide-card-container");
-}
-
-function switchCard(newIndex) {
-  hideCard();
-
-  setTimeout(() => {
-    cards[cardIndex].style.display = "none";
-    cardIndex = newIndex;
-    cards[cardIndex].style.display = "flex";
-
-    // Ajouter la classe pour afficher le reste du contenu
-    cardContainer[cardIndex].classList.add("show-card-container");
-
-    // Appeler la fonction pour afficher le reste du contenu
-    showCard();
-  }, 1000); // Attendre 1 seconde pour l'animation de masquage
-}
-
-chevronLeft.addEventListener("click", () => {
-  switchCard((cardIndex - 1 + cards.length) % cards.length);
+// Initialisation
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".chevron-right",
+    prevEl: ".chevron-left",
+  },
+  loop: true,
 });
 
-chevronRight.addEventListener("click", () => {
-  switchCard((cardIndex + 1) % cards.length);
+// Add event on click
+swiper.on("slideChangeTransitionEnd", () => {
+  updateArrowPosition();
 });
 
 // Chevron down animation
