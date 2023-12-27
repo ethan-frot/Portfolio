344fc6ec7f32e4f2aacd6deb70be317763c0446e
Ethan Frot <148550084+ethan-frot@users.noreply.github.com>
2023-12-27 14:40:34 +0100

    2.1.2
    - Add language storage in localStorage and redirection if user quits and returns
    - Added detection of the user's browser language and redirection on first connection
    - Disable default translation

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const chevronDown = document.querySelector(".chevron-down");
 const chevronRight = document.querySelector(".chevron-right");
 const chevronLeft = document.querySelector(".chevron-left");
+const languagesContent = document.querySelectorAll(".language-content");
+const userLanguage = localStorage.getItem("userLanguage");
 
-// Swiper
+// Swiper initialization
 const swiper = new Swiper(".swiper-container", {
   slidesPerView: 1,
   spaceBetween: 10,
@@ -79,3 +81,56 @@ requestAnimationFrame(updateArrowPositionLeft);
 function closeMenu() {
   document.getElementById("navi-toggle").checked = false;
 }
+
+// English or French
+function changeLanguage(lang) {
+  localStorage.setItem("userLanguage", lang);
+}
+
+// Check if user language is already set in localStorage
+if (!userLanguage) {
+  // Language not in LocalStorage, check browserLanguage
+  const browserLanguage = navigator.language.substring(0, 2);
+
+  if (browserLanguage === "fr") {
+    localStorage.setItem("userLanguage", "fr");
+    // Redirect if setting French as default
+    if (!window.location.href.includes("/public/fr.html")) {
+      window.location.href = "/public/fr.html";
+    }
+  } else {
+    localStorage.setItem("userLanguage", "en");
+    // Redirect if setting English as default
+    if (!window.location.href.includes("../")) {
+      window.location.href = "../";
+    }
+  }
+}
+
+// Determine current language from URL
+const currentLanguage = window.location.href.includes("/public/fr.html")
+  ? "fr"
+  : "en";
+
+// Redirect if user language is different from current language
+if (userLanguage !== currentLanguage) {
+  if (userLanguage === "fr" && currentLanguage === "en") {
+    window.location.href = "/public/fr.html";
+  } else if (userLanguage === "en" && currentLanguage === "fr") {
+    window.location.href = "../";
+  }
+}
+
+// Change the localStorage language when clicking
+languagesContent.forEach((languageContent) => {
+  languageContent.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (userLanguage === "fr") {
+      changeLanguage("en");
+      window.location.href = "../";
+    } else {
+      changeLanguage("fr");
+      window.location.href = "/public/fr.html";
+    }
+  });
+});
